ed8b887228f88d464fd74117977db661848998ae
miasumon <miasumon.ru@gmail.com>
2024-05-14 07:37:43 +0600

    json webtoken installed

--- index.js
+++ index.js
@@ -6,12 +6,19 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const cors = require("cors")
 
+const jwt = require('jsonwebtoken');
+
 const port = process.env.PORT || 5000
 
 // middleware
 
 
-app.use(cors())
+app.use(cors({
+  origin : [
+    "http://localhost:5173"
+  ],
+  credentials : true
+}))
 app.use(express.json())
 
 console.log(process.env.DB_USER)
@@ -29,6 +36,15 @@ const client = new MongoClient(uri, {
   }
 });
 
+// cookieOption
+
+const cookieOption = {
+  httpOnly : true,
+  sameSite : process.env.NODE_ENV === "production" ? "none" : "strict",
+  secure : process.env.NODE_ENV === "production" ? true : false
+}
+
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -36,6 +52,26 @@ async function run() {
 
     const booksCollection = client.db("booksDB").collection("books")
     const borrowedBooksCollection = client.db("booksDB").collection("borrowedBooks")
+
+
+    // auth related api
+
+    app.post('/jwt', async(req, res)=> {
+
+      const user = req.body
+
+      console.log("user for jwt", user)
+
+      const token = jwt.sign(user,  process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+      
+      res.cookie("token", token, cookieOption )
+         .send({succees : true})
+      
+
+
+
+
+    })
 
 
 
@@ -117,6 +153,91 @@ async function run() {
 
       res.send(result)
 
+    })
+
+    // 
+
+    app.get('/borrowedBooks', async(req, res)=> {
+
+      const email = req.query.email
+
+      console.log(req.url)
+
+
+      const query = { email : email };
+
+      const result =  await borrowedBooksCollection.find(query).toArray();
+
+      res.send(result)
+
+    })
+
+    app.patch('/borrowedBooks/:id', async(req, res)=> {
+
+      const id = req.params.id
+      const quantity = req.body
+      console.log( "patch for ", id, quantity)
+
+      const query = { _id : new ObjectId(id)}
+     
+      const updateDoc = {
+        $inc  : {
+          quantity : -1
+        }
+      }
+
+      const result = await booksCollection.updateOne(query, updateDoc)
+
+      res.send(result)
+
+
+
+
+    })
+
+    app.patch('/books/:bookName', async(req, res)=> {
+
+      const bookName = req.params.bookName
+      // const quantity = req.body
+      console.log( "patch for ", bookName)
+
+      const filter  = { bookName : bookName }
+
+      console.log("query for ", filter )
+
+      const options = { upsert: true };
+     
+      const updateDoc = {
+        $inc  : {
+          quantity : 1
+        }
+      }
+
+      const result = await booksCollection.updateOne(filter , updateDoc , options)
+
+      console.log(result)
+
+      res.send(result)
+
+
+
+
+    })
+
+    app.delete("/books/:id", async(req, res) => {
+      const id = req.params.id
+      console.log("id for delete " , id)
+
+      const message = req.body
+
+      console.log(message)
+
+      const query = {_id : new ObjectId(id) };
+
+      const result = await borrowedBooksCollection.deleteOne(query);
+      
+
+      res.send(result)
     })
 
 
